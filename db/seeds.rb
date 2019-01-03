82f029b091a564c4bbf28c687a627c5e75060318
jbmenashi <jbmenashi@gmail.com>
2019-01-03 14:36:56 -0500

    reached mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Event.destroy_all
-# When.destroy_all
+Event.destroy_all
+Location.destroy_all
+When.destroy_all
+
+when1 = When.create(date: DateTime.new(2018, 12, 25))
+when2 = When.create(date: DateTime.new(2018, 12, 23))
+when3 = When.create(date: DateTime.new(2018, 12, 21))
+
+loc1 = Location.create(city: "Boston", state: "MA", latitude: 42.3125, longitude: -70.9978)
+loc2 = Location.create(city: "Des Moines", state: "IA", latitude: 41.575, longitude: -93.6038)
+loc3 = Location.create(city: "Seattle", state: "WA", latitude: 47.6062, longitude: -122.3321)
+loc4 = Location.create(city: "Philadelphia", state: "PA", latitude: 39.9526, longitude: -75.1652)
+
+Event.create(when_id: when1.id, location_id: loc1.id, title: "Christmas in Boston", info: "Lots of holiday spirit!", img_url: "https://i.ndtvimg.com/i/2015-12/christmas_650x400_71450865126.jpg")
+Event.create(when_id: when2.id, location_id: loc1.id, title: "Celtics Game", info: "Basketball sure is fun!", img_url: "https://cdn.vox-cdn.com/thumbor/5w4KZ71ZASzPehmoxsv7uxovgVk=/0x0:5184x3456/1200x800/filters:focal(3433x656:4261x1484)/cdn.vox-cdn.com/uploads/chorus_image/image/61852583/1039402776.jpg.0.jpg")
+Event.create(when_id: when3.id, location_id: loc1.id, title: "Walked the Freedom Trail", info: "I learned about the founding fathers", img_url: "http://res.cloudinary.com/simpleview/image/upload/v1459385393/clients/boston/freedomtrail-medallion-massport-cl-11_e57a98c5-f0ae-5102-046b682397fbe73d.jpg")
+Event.create(when_id: when2.id, location_id: loc2.id, title: "Butter Sculpture Festival", info: "Butter is delicious!", img_url: "https://cdn0.wideopeneats.com/wp-content/uploads/2017/09/butter-sculptures-fi.png")
+Event.create(when_id: when2.id, location_id: loc3.id, title: "Climbed the Space Needle", info: "It's very tall", img_url: "http://deacademic.com/pictures/dewiki/83/SpaceNeedleTopClose.jpg")
+Event.create(when_id: when3.id, location_id: loc4.id, title: "Cracked the Liberty Bell", info: "Yikes.", img_url: "http://www.ushistory.org/libertybell/images/bell3.jpg")
--- hapndhere_frontend/src/app.js
+++ hapndhere_frontend/src/app.js
@@ -4,6 +4,16 @@ class App {
     let foundLocation
     let foundWhen
 
+    let mymap = L.map('map-area').setView([39, -96], 4);
+    let markersLayerGroup = L.layerGroup()
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox.streets',
+      accessToken: 'pk.eyJ1IjoiamJtZW5hc2hpIiwiYSI6ImNqcWZyYXFvZTU1ejg0MmxnNXQxaHZwYTMifQ.Uvhck3cxhndzZdytkylSPw'
+  }).addTo(mymap);
+
     const fetchEventsByLocation = (when, location) => {
       fetch(`http://localhost:3000/api/v1/whens/${when.id}/locations/${location.id}/events`)
       .then(response => response.json())
@@ -16,6 +26,8 @@ class App {
       })
     }
 
+
+
     document.addEventListener('submit', (event) => {
       event.preventDefault()
       if (event.target.id === "date-submit") {
@@ -24,17 +36,28 @@ class App {
           fetch(`http://localhost:3000/api/v1/whens/${foundWhen.id}/locations`)
           .then(response => response.json())
           .then(data => {
-            document.querySelector('#list-of-locations').innerHTML = ''
+            mymap.on('click', e => {
+              document.querySelector('#add-event-form').reset()
+              let infoPopup = L.popup()
+              infoPopup.setLatLng(e.latlng).setContent("Add An Event To This Location Below!").openOn(mymap)
+              document.querySelector('#lat-input').value = e.latlng.lat
+              document.querySelector('#long-input').value = e.latlng.lng
+            });
+            markersLayerGroup.clearLayers()
+            document.querySelector('#add-event-form').reset()
             document.querySelector('#list-of-events').innerHTML = ''
             document.querySelector('.event-info').innerHTML = `<h3>Event Info:</h3>`
-            document.querySelector('#city-input').value = ''
-            document.querySelector('#state-input').value = ''
-            document.querySelector('#lat-input').value = ''
-            document.querySelector('#long-input').value = ''
             data.forEach(location => {
-              document.querySelector('#list-of-locations').innerHTML += `
-                                                                <li data-id="${location.id}">${location.city}</li>
-                                                                `
+              let marker = L.marker([location.latitude, location.longitude]);
+              markersLayerGroup.addLayer(marker).addTo(mymap)
+              marker.on('click', e => {
+                marker.bindPopup(`${location.city}, ${location.state}`).openPopup();
+                document.querySelector('#city-input').value = location.city
+                document.querySelector('#state-input').value = location.state
+                document.querySelector('#lat-input').value = location.latitude
+                document.querySelector('#long-input').value = location.longitude
+                fetchEventsByLocation(foundWhen, location)
+              })
             })
           })
         }
@@ -55,7 +78,6 @@ class App {
           .then(response => response.json())
           .then(data => {
             When.all.push(data)
-            document.querySelector('#list-of-locations').innerHTML = ''
           })
         }
       }
@@ -104,6 +126,16 @@ class App {
           .then(response => response.json())
           .then(data => {
             Location.all.push(data)
+            let newMarker = L.marker([data.latitude, data.longitude])
+            markersLayerGroup.addLayer(newMarker)
+            newMarker.on('click', e => {
+              newMarker.bindPopup(`${data.city}, ${data.state}`).openPopup();
+              document.querySelector('#city-input').value = data.city
+              document.querySelector('#state-input').value = data.state
+              document.querySelector('#lat-input').value = data.latitude
+              document.querySelector('#long-input').value = data.longitude
+              fetchEventsByLocation(foundWhen, data)
+            })
             fetch(`http://localhost:3000/api/v1/events`, {
               method: 'POST',
               headers: {
@@ -129,21 +161,23 @@ class App {
     })
 
     document.addEventListener('click', (event) => {
-      if (event.target.parentNode.id === "list-of-locations") {
-        foundWhen = When.all.find(when => when.date.includes(`${document.querySelector('#start').value}`))
-        foundLocation = Location.all.find(location => location.id == event.target.dataset.id)
-        document.querySelector('#city-input').value = foundLocation.city
-        document.querySelector('#state-input').value = foundLocation.state
-        document.querySelector('#lat-input').value = foundLocation.latitude
-        document.querySelector('#long-input').value = foundLocation.longitude
-        fetchEventsByLocation(foundWhen, foundLocation)
-      }
-      else if (event.target.parentNode.id === "list-of-events") {
+      if (event.target.parentNode.id === "list-of-events") {
         let foundEvent = Event.all.find(e => e.id == event.target.dataset.id)
         document.querySelector('.event-info').innerHTML = `<div id="event-info-deets"><h3>${foundEvent.title}</h3>
                                                             <h4>${foundEvent.info}</h4></div>
                                                             <div id="event-info-image"><img src="${foundEvent.img_url}" width="200px" height="200px">
+                                                            </div>
+                                                            <div id="buttons" data-id="${event.target.dataset.id}">
+                                                              <button id="edit-event" class="btn btn-warning btn-sm">Edit</button>
+                                                              <button id="delete-event" class="btn btn-danger btn-sm">Delete</button>
                                                             </div>`
+      }
+      else if (event.target.id === "edit-event") {
+        let foundEvent = Event.all.find(e => e.id == event.target.parentNode.dataset.id);
+        
+      }
+      else if (event.target.id === "delete-event") {
+        let foundEvent = Event.all.find(e => e.id == event.target.parentNode.dataset.id);
       }
     })
 
